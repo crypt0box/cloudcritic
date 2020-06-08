b8d7e5281efe4788f6e778feda777826912c64ef
crypt0box <pray.happy76@gmail.com>
2020-06-08 12:27:43 +0900

    UPDATE: use GoogleBooksAPI

--- pages/items.vue
+++ pages/items.vue
@@ -14,51 +14,17 @@
             v-for="item in $store.getters['items/getResults']"
             :key="item.id"
           >
-            <v-card>
+          <a :href="item.volumeInfo.previewLink">
             <v-img
-              :src="item.Item.mediumImageUrls[0].imageUrl"
+              v-if="item.volumeInfo.imageLinks != undefined"
+              :src="item.volumeInfo.imageLinks.thumbnail"
               class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
             >
             </v-img>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-            </v-card>
+          </a>
           </v-col>
         </v-row>
       </v-container>
-    </div>
-    <div>
-      <ul class="resultItems">
-        <li class="resultItem" v-for="item in $store.getters['items/getResults']" :key="item.id">
-          <img :src="item.Item.mediumImageUrls[0].imageUrl" class="imgStyle">
-          <dl class="dataStyle">
-            <dt>
-              <a :href="item.Item.itemUrl">
-                Name: {{ item.Item.itemName.slice(0,20) + "..." }}
-              </a>
-            </dt>
-            <dd class="price">
-              ¥{{ item.Item.itemPrice }}
-            </dd>
-          </dl>
-        </li>
-      </ul>
     </div>
   </section>
 </template>
--- store/items.js
+++ store/items.js
@@ -10,13 +10,12 @@ export const mutations = {
 
 export const actions = {
   async search({ commit }, itemName) {
-    const baseUrl = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?'
-    const appId = 'applicationId=1061213804419554354'
-    let keyword = `&keyword=${itemName}`
-    const getUrl = encodeURI(baseUrl + appId + keyword)
+    const baseUrl = 'https://www.googleapis.com/books/v1/volumes?q='
+    let keyword = `${itemName}`
+    const getUrl = baseUrl + keyword
     const response = await this.$axios.$get(getUrl)
     console.log(response)
-    commit('setResults', response.Items)
+    commit('setResults', response.items)
   }
 }
 
