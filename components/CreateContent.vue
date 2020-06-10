7f6c3490404ef2a601f535272a52eed2c1671e80
crypt0box <pray.happy76@gmail.com>
2020-06-10 09:42:36 +0900

    UPDATE: minor modifications

---
+++ components/CreateContent.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <input v-model="name">
+  <v-btn @click="search">search</v-btn>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="item in $store.getters['items/getResults']"
+        :key="item.id"
+      >
+      <a :href="item.volumeInfo.previewLink">
+        <v-img
+          v-if="item.volumeInfo.imageLinks != undefined"
+          :src="item.volumeInfo.imageLinks.thumbnail"
+          maxWidth="100"
+          maxHeight="120"
+          class="white--text align-end"
+        >
+        </v-img>
+      </a>
+      </v-col>
+    </v-row>
+  </v-container>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+    }
+  },
+  methods: {
+    search() {
+      this.$store.dispatch('items/search', this.name)
+    },
+  }
+}
+</script>
--- pages/items.vue
+++ pages/items.vue
@@ -4,38 +4,20 @@
       <nuxt-link :to="'users/' + $store.getters['auth/getUserUid']">user</nuxt-link>
       <button @click="show">クリックする</button>
 
-      <modal name="modal-content">
-          <p>モーダルウィンドウで表示されるコンテンツ</p>
+      <modal name="modal-content" height="80%" width="80%" :scrollable="true">
+        <create-content />
       </modal>
-    </div>
-    <div>
-      <input v-model="name">
-      <v-btn @click="search">search</v-btn>
-    </div>
-    <div>
-      <v-container>
-        <v-row>
-          <v-col
-            v-for="item in $store.getters['items/getResults']"
-            :key="item.id"
-          >
-          <a :href="item.volumeInfo.previewLink">
-            <v-img
-              v-if="item.volumeInfo.imageLinks != undefined"
-              :src="item.volumeInfo.imageLinks.thumbnail"
-              class="white--text align-end"
-            >
-            </v-img>
-          </a>
-          </v-col>
-        </v-row>
-      </v-container>
     </div>
   </section>
 </template>
 
 <script>
+import CreateContent from '~/components/CreateContent.vue'
+
 export default {
+  components: {
+    CreateContent
+  },
   data() {
     return {
       name: '',
