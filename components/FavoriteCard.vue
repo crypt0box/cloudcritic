7542bffd8fa7dbdcc9ae9fdf3af7e4b7d0a0c653
crypt0box <pray.happy76@gmail.com>
2020-07-23 11:13:56 +0900

    UPDATE: display only favorite content

--- components/FavoriteCard.vue
+++ components/FavoriteCard.vue
@@ -1,41 +1,39 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col
-        v-for="content in contents"
-        :key="content.id"
-        cols="6"
-        md="3"
+  <v-row>
+    <v-col
+      v-for="content in filterContents"
+      :key="content.id"
+      cols="6"
+      md="3"
+    >
+      <v-card
+        maxWidth="150"
+        maxHeight="270"
+        width="150"
+        height="270"
       >
-        <v-card
-          maxWidth="150"
-          maxHeight="270"
-          width="150"
-          height="270"
-        >
-          <v-row justify="center">
-            <nuxt-link :to="{path: `../content/${content.id}`, params: { contentID: content.id }}">
-            <v-img
-              :src="content.thumbnailUrl"
-              maxWidth="150"
-              maxHeight="220"
-              width="150"
-              height="220"
-            />
-            </nuxt-link>
-          </v-row>
-          <v-row ustify="center">
-            <v-col>
-              <v-icon center>mdi-thumb-up</v-icon> {{ content.totalLike }}
-            </v-col>
-            <v-col>
-              <v-icon center>mdi-heart</v-icon>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+        <v-row justify="center">
+          <nuxt-link :to="{path: `../content/${content.id}`, params: { contentID: content.id }}">
+          <v-img
+            :src="content.thumbnailUrl"
+            maxWidth="150"
+            maxHeight="220"
+            width="150"
+            height="220"
+          />
+          </nuxt-link>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-icon center>mdi-thumb-up</v-icon> {{ content.totalLike }}
+          </v-col>
+          <v-col>
+            <v-icon center>mdi-heart</v-icon>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -46,8 +44,9 @@ export default {
     this.$store.dispatch('auth/onAuth')
   },
   computed: {
-    contents() {
+    filterContents() {
       return this.$store.getters['contents/getContents']
+        .filter(content => this.$store.getters['auth/getFavorite'].includes(content.id))
     },
   }
 }
