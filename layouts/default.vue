3d97833f747912714c593b14b1ac1cecf1cb2b1e
crypt0box <pray.happy76@gmail.com>
2020-08-02 17:30:37 +0900

    UPDATE: add logo in nav-bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,32 +30,12 @@
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+        <v-avatar :tile="true" size="180">
+          <img  
+            src="~/static/cloudcritic_logo.png"
+            alt="logo"
+          >
+        </v-avatar>
     </v-app-bar>
     <v-content>
       <v-container>
