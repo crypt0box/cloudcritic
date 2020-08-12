6985f0515d2a17f2c4ba11b223c6b3fcd4eea188
crypt0box <pray.happy76@gmail.com>
2020-08-12 16:19:12 +0900

    ADD: add home page

---
+++ pages/home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="header-img">
+    <div class="header-text">
+      <h1>CloudCritic</h1><br>
+      <h3>「いいね」するだけでブックレビューができるサイトです</h3>
+    </div>
+    <div class="header-form">
+      <v-text-field
+        solo
+        label="search"
+        append-icon="mdi-magnify"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'home',
+}
+</script>
+
+<style scoped>
+.header-img {
+  height: 300px;
+  width: 100%;
+  background-image: url("../static/home_image.jpg");
+  background-size: cover;
+}
+.header-text {
+  padding: 5%;
+  color: white;
+  text-shadow: 1px 1px 10px gray;
+}
+.header-form {
+    padding-left: 5%;
+    padding-right: 5%;
+  }
+@media screen and (max-width: 480px) {
+  .header-img {
+    height: 300px;
+    width: 100%;
+    background-image: url("../static/home_image.jpg");
+    background-size: cover;
+  }
+  .header-text {
+    padding: 5%;
+    text-align: center;
+    color: white;
+    text-shadow: 1px 1px 10px gray;
+  }
+  .header-form {
+    padding-left: 5%;
+    padding-right: 5%;
+  }
+}
+</style>
