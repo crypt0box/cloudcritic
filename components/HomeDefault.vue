5f6822c2559f2ec5b290619ac1c4c4d109dac518
crypt0box <pray.happy76@gmail.com>
2020-08-28 20:37:48 +0900

    UPDATE: improvement of UI

--- components/HomeDefault.vue
+++ components/HomeDefault.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+  <div>
     <div
       class="header-img"
       :style="{height: height * 0.7 + 'px'}"
@@ -27,6 +28,52 @@
       </v-container>
     </div>
   </div>
+  <div class="body-title">
+    <h2>CloudCriticとは</h2>
+  </div>
+    <div style="padding-left: 5%; padding-right: 5%;">
+      <v-row justify="center">
+        <v-col
+          v-for="card in cards"
+          :key="card.id"
+          cols=4
+        >
+          <v-card 
+            width="100%"
+            height="100%"
+            style="padding: 2%;"
+          >
+            <v-row justify="center">
+              <v-avatar size="80">
+                <v-icon x-large>{{ card.icon }}</v-icon>
+              </v-avatar>
+            </v-row>
+            <v-row justify="center">
+              <v-card-title>
+                <h3>{{ card.title }}</h3>
+              </v-card-title>
+            </v-row>
+            <v-row justify="center">
+              <v-card-text>
+                {{ card.text }}
+              </v-card-text>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+    <div 
+      style="padding: 3%;">
+      <v-row justify="center">
+          <v-btn 
+            color="#FFCB00"
+            @click="register"
+          >
+          新規登録してはじめる
+          </v-btn>
+      </v-row>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -50,12 +97,34 @@ export default {
   data() {
     return {
       height: 0,
-      innerWidth: window.innerWidth
+      innerWidth: window.innerWidth,
+      contents: [],
+      model: 0,
+      cards: {
+        first: {
+          icon:'mdi-pencil',
+          title: '簡単レビュー',
+          text: '自分の感想を言語化するのを難しく感じたことはありませんか？書籍についたタグに「いいね」するだけで簡単にレビューできます。'
+        },
+        second: {
+          icon:'mdi-cloud',
+          title: 'ワードクラウド',
+          text: 'タグのいいね数に応じてワードクラウドを生成。書籍の評価されているポイントが一目でわかります。'
+        },
+        third: {
+          icon:'mdi-cog',
+          title: 'シンプルなデザイン',
+          text: 'シンプルで無駄のないデザインが直感的な操作を可能にします。レスポンシブ対応のため、スマホでもPCでも使えます。'
+        },
+      }
     }
   },
   methods: {
     handleResize () {
       this.height = window.innerHeight
+    },
+    register() {
+      this.$router.push('/register')
     }
   }
 }
@@ -78,5 +147,8 @@ export default {
 .body-title {
   padding: 1%;
   padding-left: 5%;
+}
+.body-content {
+  padding-top: 10%;
 }
 </style>
