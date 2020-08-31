b4c90e5a4c2d3d0e7f8ffff4f8784c8d5570c581
crypt0box <pray.happy76@gmail.com>
2020-08-31 20:03:50 +0900

    UPDATE: update wordcloud option

--- components/WordCloud.vue
+++ components/WordCloud.vue
@@ -4,6 +4,9 @@
     :data="words"
     name-key="name"
     value-key="like"
+    :rotate="rotate"
+    :margin="margin"
+    :fontSize="fontSize"
     :color="myColors"
     :show-tooltip="false"
   />
@@ -16,6 +19,9 @@ export default {
   data() {
     return {
       dialog: false,
+      rotate: {from: 0, to: 0, numOfOrientation: 0 },
+      margin: {top: 50, right: 50, bottom: 50, left: 50 },
+      fontSize: [20, 80],
       myColors: ['#38b508', '#76ed47', '#a8e88f', '#39c900'],
       words: JSON.parse(JSON.stringify(this.$store.getters['contents/getTags'])),
     }
