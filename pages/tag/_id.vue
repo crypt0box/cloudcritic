6273e42115a2f8fa77955395a1e8b920520942b6
crypt0box <pray.happy76@gmail.com>
2020-09-06 10:13:37 +0900

    UPDATE: Implementation of tag search function

--- pages/tag/_id.vue
+++ pages/tag/_id.vue
@@ -3,19 +3,26 @@
     <body>
       <v-container>
         <v-row>
-          {{ contents[0].name }}
+          <h1>タグ：{{ contents[0].name }}</h1>
         </v-row>
         <v-divider />
         <v-row style="padding-top: 3%;">
           <v-col
-            v-for="content in contents"
+            v-for="(content, index) in contents"
             :key="content.id"
             cols="6"
             md="2"
           >
           <v-row justify="center">
             <nuxt-link :to="'../content/' + content.contentId">
-              {{ content.contentId }}
+              <v-img
+                :src="thumbnailUrl[index]"
+                maxWidth="100"
+                maxHeight="140"
+                width="100"
+                height="140"
+              >
+              </v-img>
             </nuxt-link>
           </v-row>
           <v-row justify="center">
@@ -35,7 +42,25 @@
 </template>
 
 <script>
+import firebase from '~/plugins/firebase'
+
+const db = firebase.firestore()
+const contentsRef = db.collection('contents')
+
 export default {
+  data() {
+    return {
+      thumbnailUrl: []
+    }
+  },
+  created() {
+    this.$store.getters['search/getContents'].forEach(element => {
+      contentsRef.doc(element.contentId).get()
+      .then(doc => {
+        this.thumbnailUrl.push(doc.data().thumbnailUrl)
+      })
+    });
+  },
   computed:{
     contents() {
       return this.$store.getters['search/getContents']
